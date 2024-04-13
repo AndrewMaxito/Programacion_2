7093c99796833992547f4affb8ba3aaf3af52305
MaxB. <andremaxbg18@gmail.com>
2024-04-12 21:00:29 -0500

    pipi

--- Plantillas/PlantillaDeFunciones.cpp
+++
@@ -1,5 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.cc to edit this template
- */
-
