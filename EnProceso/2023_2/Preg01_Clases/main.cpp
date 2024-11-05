a7ee6a5a698243176a20bd616c63c986fc3d21c4
MaxB. <150452456+AndrewMaxito@users.noreply.github.com>
2024-11-05 16:15:17 -0500

    .

---
+++ EnProceso/2023_2/Preg01_Clases/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: andre
+ *
+ * Created on 5 de noviembre de 2024, 16:10
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
