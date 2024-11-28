5f067b2a2083873032faabe70ccc5d78d0ff18a1
MaxB. <150452456+AndrewMaxito@users.noreply.github.com>
2024-11-27 20:40:57 -0500

    .

---
+++ EnProceso/STL/2023_2/LAB09_PREG01/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: andre
+ *
+ * Created on 27 de noviembre de 2024, 02:09 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
